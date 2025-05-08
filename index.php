0e34d7b9e4b78694cf74bbd1d4bc9d6747aafec2
Yago <95967079+Yagohsl@users.noreply.github.com>
2025-05-08 09:20:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $receitas = $stmt->fetchAll();
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
 
-  <title>Receitas de Stardew</title>
+  <title>Stardew Receitas</title>
 </head>
 <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top" >
   <div class="container-fluid">
